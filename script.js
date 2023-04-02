c4ef88cd528fd7501ba1f328f8bc7297df1bf333
Nimesh <nimeshkayastha067@gmail.com>
2023-04-02 12:50:50 +0545

    sign up form complete

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ submitButton.addEventListener("click",function(e){
     // console.log(firstnameinput.value);
     let lastnameinput=signupform.lastname;
 
+    let emailinput=signupform.email;
+
+    let passwordinput=signupform.password;
+
     if(firstnameinput.value ==""){
         errors.firstname="Firstname should not be empty"
         
@@ -18,14 +22,34 @@ submitButton.addEventListener("click",function(e){
     if(lastnameinput.value ==""){
         errors.lastname="lastname should not be empty";     
     }
+
+    if(emailinput.value ==""){
+        errors.email="email should not be empty";     
+    }
+
+    if(passwordinput.value ==""){
+        errors.password="password should not be empty ";     
+    }
+    
+    if(passwordinput.value.length<=5){
+        errors.password="password must be minimum of 5 length"; 
+    }
+
     console.log(errors);
     for(let error in errors){
-
+        let lastnamegroup=document.getElementById(`${error}group`);
+        let lastnamemessage=document.querySelector( `#${error}group .message`);
+        lastnamegroup.classList.add("error");
+        lastnamemessage.textContent=errors[error];
+    
     }
+
+if(Object.keys(errors).length==0){
+    signupform.submit();
+
+
+}
+
 });
 
 
-// let lastnamegroup=document.getElementById("lastnamegroup");
-// let lastnamemessage=document.querySelector("#lastnamegroup .message");
-// lastnamegroup.classList.add("error");
-// lastnamemessage.textContent="lastname should not be empty";
