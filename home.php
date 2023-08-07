2ad2679a0b72d3b25f352adcbbfd87034b38a2f3
Nimesh <nimeshkayastha067@gmail.com>
2023-08-07 13:11:46 +0545

    fini

--- home.php
+++ home.php
@@ -21,6 +21,144 @@
     </nav>
     <div id="menu-btn" class="fas fa-bars"></div>
 </section>
+<!-- header end -->
+
+<!-- Home section start -->
+
+<section class="home">
+    <div class="swiper home-slider">
+        <div class="swiper-wrapper">
+                <div class="swiper-slide slide" style="background:url() no repeat">
+                    <div class="content">
+                        <span>explore,discover,travel</span>
+                        <h3>travel around the world</h3>
+                        <a href="package.php" class="btn">discover more</a>
+                    </div>
+            </div>
+        
+            <div class="swiper-slide slide" style="background:url() no repeat">
+                    <div class="content">
+                        <span>explore,discover,travel</span>
+                        <h3>discover new places</h3>
+                        <a href="package.php" class="btn">discover more</a>
+                    </div>
+            </div>
+
+            <div class="swiper-slide slide " style="background:url() no repeat">
+                    <div class="content">
+                        <span>explore,discover,travel</span>
+                        <h3>make ypur tour worthwhile</h3>
+                        <a href="package.php" class="btn">discover more</a>
+                    </div>
+            </div>
+
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+    </div>
+
+</section>
+<!-- home sec end -->
+
+<!-- service section start-->
+<section class="services">
+    <h1 class="heading-title">our services</h1>
+    
+    <div class="box-container">
+        <div class="box">
+            <img src="images/icon-1.png" alt="">
+            <h3>adventure</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-2.png" alt="">
+            <h3>tour guide</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-3.png" alt="">
+            <h3>trekking</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-4.png" alt="">
+            <h3>camp fire</h3>
+        </div>
+
+        
+    </div>
+</section>
+<!-- service section end-->
+
+
+<!-- home about sec start -->
+<section class="home-about">
+    <div class="image">
+        <img src="" alt="" >
+    </div>
+
+    <div class="content">
+        <h3>about us</h3>
+        <p>                </p>
+        <a href="about.php" class="btn">read more</a>
+    </div>
+</section>
+
+<!-- home about sec end -->
+
+<!-- home package sec start -->
+<section class="home-packages">
+    <h1 class="heading"> our packages</h1>
+    <div class="box-container">
+        <div class="box">
+            <div class="image">
+                <img src="" alt="" >
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>                        </p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <!-- 2nd img -->
+        <div class="box">
+            <div class="image">
+                <img src="" alt="" >
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>                        </p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <!-- 3rd image -->
+        <div class="box">
+            <div class="image">
+                <img src="" alt="" > 
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>                        </p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+    </div>
+
+<div class="load-more"> <a href="package.php" class="btn">load more</a></div>
+</section>
+
+<!-- home package sec end -->
+
+<!-- home offer sec start -->
+<section class="home-offer">
+    <div class="content">
+        <h3>upto 50% off</h3>
+        <p>              </p>
+        <a href="book.php" class="btn">book now</a>
+    </div>
+</section>
+<!-- home offer sec end -->
 
 
 
@@ -59,8 +197,11 @@
 </div>
 
 </div>
+
+<div class="credit">created by <span>mr.web designer</span> | all rights reserved! </div>
 </section>
 
+<!-- footer sec ends -->
 
      <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
     <script src="js/script.js"></script>
